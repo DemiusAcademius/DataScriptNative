1b5e3162399b4b41203bc4e0d234207207c7fe3f
unknown <demius@wks0556.apa-canal.md>
2016-01-05 15:09:30 +0200

    helper functions for ResultSet, PreparedStatement etc

--- src/com/acc/datascript/generator/helpers/commonHelper.kt
+++ src/com/acc/datascript/generator/helpers/commonHelper.kt
@@ -1,88 +1,91 @@
 package com.acc.datascript.generator.helpers
 
+import com.acc.datascript.lang.names.name
+import com.acc.datascript.lang.types.DataScriptType
+import com.acc.datascript.resources.ColumnType
+
 /**
  * Created by demius on 05.01.2016.
  */
 
-def resultSetGetter(DataScriptType type, String sqlVar) {
-    switch type {
-        DataScriptEnumType: {
-        '''«type.enumRef.name».select(«sqlVar»,1)'''
-    }
-        default: {
-        val sqlType = type.sqlType
-        val ts = sqlType.partOfExpression
-        val postfix = if (sqlType == ColumnType.BOOLEAN) " == 1" else ""
-        val v = '''«sqlVar».get«ts»(1)«postfix»'''
-        if (sqlType.nullableNumeric) {
-            '''
-            {
-                «sqlType.serviceDeclaration» v = «v»;
-                «sqlVar».wasNull() ? null : v;
-            }'''
-        } else {
-            '''«v»'''
-        }
-    }
-    }
-}
-
-def resultSetGetter(DataScriptType type, String name, int index, String prefix, boolean trans_utf) {
-    switch type {
-        DataScriptEnumType: {
-        if (prefix != null) '''
-        «prefix».«name» = «type.enumRef.name».select(rs,«index»);
-        '''
-        else '''«type.enumRef.name».select(rs,«index»)'''
-    }
-        default: {
-        val sqlType = type.sqlType
-        val ts = sqlType.partOfExpression
-        val postfix = if (sqlType == ColumnType.BOOLEAN) " == 1" else ""
-        val v = '''rs.get«ts»(«index»)«postfix»'''
-        if (sqlType.nullableNumeric) {
-            '''
-            {
-                «sqlType.serviceDeclaration» v = «v»;
-                «IF prefix != null»«prefix».«name» = rs.wasNull() ? null : v;«ELSE»return rs.wasNull() ? null : v;«ENDIF»
-            }«IF prefix != null»;
-            «ENDIF»'''
-        } else {
-            val vv = if (trans_utf) '''com.acc.server.utils.TranslateRomUtf.translate(«v»)''' else v
-            if (prefix != null) '''
-            «prefix».«name» = «vv»;
-            '''
-            else '''«vv»'''
-        }
-    }
-    }
-}
-
-def statementSetter(DataScriptType type, String name, int index, String source) {
-    switch type {
-        DataScriptEnumType: {
-        '''
-        «source»«name».setValue(st,«index»);
-        '''
-    }
-        default: {
-        val sqlType = type.sqlType
-        val ts = sqlType.partOfExpression
-        if (sqlType.nullableNumeric) {
-            '''
-            if («source»«name» == null) {
-                st.setNull(«index»,java.sql.Types.INTEGER);
-            } else {
-                st.set«ts»(«index»,«source»«name».«sqlType.serviceDeclaration»Value());
+public fun sqlGetter(type: DataScriptType, sqlVar: String): CharSequence =
+    when (type) {
+        is DataScriptType.Enum -> "${type.enumRef.name}.select($sqlVar,1)"
+        is DataScriptType.Sql -> {
+            val sqlType = type.sqlType
+            if (sqlType == null) ""
+            else {
+                val ts = sqlType.partOfExpression()
+                val postfix = if (sqlType == ColumnType.BOOLEAN) " == 1" else ""
+                val v = "$sqlVar.get$ts(1)$postfix"
+                if (sqlType.isNullableNumeric)
+                    """
+                    {
+                        ${sqlType.serviceDeclaration()} v = $v;
+                        $sqlVar.wasNull() ? null : v;
+                    }"""
+                else v
             }
-            '''
-        } else {
-            val postfix = if (sqlType == ColumnType.BOOLEAN) " ? 1 : 0" else ""
-            '''
-            st.set«ts»(«index»,«source»«name»«postfix»);
-            '''
         }
     }
-    }
-}
 
+public fun sqlGetter(type: DataScriptType, name: String, index: Int, prefix: String?, transUtf: Boolean): CharSequence =
+        when (type) {
+            is DataScriptType.Enum ->
+                if (prefix != null) """
+                    $prefix.$name = ${type.enumRef.name}.select(rs,$index);
+                    """
+                else "${type.enumRef.name}.select(rs,$index)"
+            is DataScriptType.Sql -> {
+                val sqlType = type.sqlType
+                if (sqlType == null) ""
+                else {
+                    val ts = sqlType.partOfExpression()
+                    val postfix = if (sqlType == ColumnType.BOOLEAN) " == 1" else ""
+                    val v = "rs.get$ts($index)$postfix"
+                    if (sqlType.isNullableNumeric) {
+                        val sp = if (prefix != null) "$prefix.$name = " else "return "
+                        val se = if (prefix != null) ";\n" else ""
+                        """
+                        {
+                        ${sqlType.serviceDeclaration()} v = $v;
+                        $sp = rs.wasNull() ? null : v;
+                        }$se"""
+                    }
+                    else {
+                        val vv = if (transUtf) "com.acc.server.utils.TranslateRomUtf.translate($v)" else v
+                        if (prefix != null) """
+                            $prefix.$name = $vv;
+                            """
+                        else vv
+                    }
+                }
+            }
+        }
+
+public fun statementSetter(type: DataScriptType, name: String, index: Int, source: String): CharSequence =
+        when (type) {
+            is DataScriptType.Enum -> """
+                $source$name.setValue(st,$index);
+                """
+            is DataScriptType.Sql -> {
+                val sqlType = type.sqlType
+                if (sqlType == null) ""
+                else {
+                    val ts = sqlType.partOfExpression()
+                    if (sqlType.isNullableNumeric) """
+                        if ($source$name == null) {
+                            st.setNull($index, java.sql.Types.INTEGER);
+                        } else {
+                            st.set$ts($index, $source$name.${sqlType.serviceDeclaration()}Value());
+                        }
+                        """
+                    else {
+                        val postfix = if (sqlType == ColumnType.BOOLEAN) " ? 1 : 0" else ""
+                        """
+                        st.set$ts($index,$source$name$postfix);
+                        """
+                    }
+                }
+            }
+        }
